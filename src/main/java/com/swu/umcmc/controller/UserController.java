02d1dee3b272dd05598c8cd9e059fb88a92bfbe0
zwei1garden <dlwjddnjs081723@naver.com>
2024-11-24 05:16:30 +0900

    feat #17 : cors 설정 완료

--- src/main/java/com/swu/umcmc/controller/UserController.java
+++ src/main/java/com/swu/umcmc/controller/UserController.java
@@ -5,15 +5,12 @@ import com.swu.umcmc.dto.user.UserRequestDto;
 import com.swu.umcmc.dto.user.UserResponseDto;
 import com.swu.umcmc.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
