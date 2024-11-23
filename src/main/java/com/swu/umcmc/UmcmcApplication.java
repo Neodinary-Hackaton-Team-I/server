787345a6332e60d9170776a937f9b3c99d07c521
zwei1garden <dlwjddnjs081723@naver.com>
2024-11-23 17:12:44 +0900

    init

---
+++ src/main/java/com/swu/umcmc/UmcmcApplication.java
@@ -0,0 +1,13 @@
+package com.swu.umcmc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmcmcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UmcmcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/swu/umcmc/UmcmcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swu.umcmc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmcmcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
