60bfb3cafdeaf347721395ca161653283c6e9c94
dainnida <dain0928@g.hongik.ac.kr>
2024-11-23 23:35:44 +0900

    feat #3 : presigned URL 발급 구현

---
+++ src/main/java/com/swu/umcmc/domain/Presigned.java
@@ -0,0 +1,14 @@
+package com.swu.umcmc.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class Presigned {
+    private String url;
+}
