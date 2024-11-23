5341b62d015b7f117c480434f11b3c7b99c12418
zwei1garden <dlwjddnjs081723@naver.com>
2024-11-23 19:52:56 +0900

    init: swagger 및 rds 설정

---
+++ src/main/java/com/swu/umcmc/apiPayload/PageResponse.java
@@ -0,0 +1,4 @@
+package com.swu.umcmc.apiPayload;
+
+public class PageResponse {
+}
---
+++ src/main/java/com/swu/umcmc/config/exception/ErrorCode.java
@@ -0,0 +1,17 @@
+package com.swu.umcmc.config.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum ErrorCode {
+    // Common
+    INVALID_INPUT_VALUE(400, "C001", "잘못된 입력값입니다."),
+    RESOURCE_NOT_FOUND(404, "C002", "리소스를 찾을 수 없습니다."),
+    INTERNAL_SERVER_ERROR(500, "C003", "서버 에러가 발생했습니다.");
+
+    private final int status;
+    private final String code;
+    private final String message;
+}
