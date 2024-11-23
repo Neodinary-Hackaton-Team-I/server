1b96e1ef4516487afbd79b5cdb2b9975c3ff6b4c
zwei1garden <dlwjddnjs081723@naver.com>
2024-11-24 04:35:21 +0900

    feat #15 : 회원가입 응답 수정

--- src/main/java/com/swu/umcmc/dto/follow/FollowRequestDto.java
+++ src/main/java/com/swu/umcmc/dto/follow/FollowRequestDto.java
@@ -1,8 +1,6 @@
 package com.swu.umcmc.dto.follow;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
